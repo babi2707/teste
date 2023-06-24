16cac0622204a02b82520f653c03b706c3ee5281
babi2707 <90732683+babi2707@users.noreply.github.com>
2023-06-23 23:00:04 -0300

    firebase2

--- teste/firebase.js
+++ teste/firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBUkPCCXFi99DW2UPnyMAJpFKtqDBLo2e4",
-  authDomain: "todo-barbara.firebaseapp.com",
-  projectId: "todo-barbara",
-  storageBucket: "todo-barbara.appspot.com",
-  messagingSenderId: "839368344937",
-  appId: "1:839368344937:web:35e93af088660b0b34e9f8",
-  measurementId: "G-2ZM4RFHQLY",
-};
+    apiKey: "AIzaSyBUkPCCXFi99DW2UPnyMAJpFKtqDBLo2e4",
+    authDomain: "todo-barbara.firebaseapp.com",
+    projectId: "todo-barbara",
+    storageBucket: "todo-barbara.appspot.com",
+    messagingSenderId: "839368344937",
+    appId: "1:839368344937:web:35e93af088660b0b34e9f8",
+    measurementId: "G-2ZM4RFHQLY"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
